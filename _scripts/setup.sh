a12b0755d13718b73a7a8044638aeef719edf671
Adnan Memic <memica@coventry.ac.uk>
2020-02-16 20:02:58 +0000

    setup2

--- _scripts/setup.sh
+++ _scripts/setup.sh
@@ -1,5 +1,10 @@
 sudo apt-get update
-sudo apt-get install python3-venv
 
 mkdir location_app/app/databases
+
+sudo apt-get install python3-venv
 python3 -m venv location_app/venv
+
+cd location_app
+. venv/bin/activate
+pip install -r requirements.txt
