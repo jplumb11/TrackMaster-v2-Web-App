dc82675d8753371d9640b95dd078e71c21b5f902
Adnan Memic <memica@coventry.ac.uk>
2020-03-14 17:20:07 +0000

    visual fix

--- location_app/app/__init__.py
+++ location_app/app/__init__.py
@@ -2,12 +2,18 @@ from flask import Flask
 from app import mqtt, routes
 
 
-app = Flask(__name__)
-app.config.from_envvar('MYAPP_SETTINGS')
+def make_app():
+    """
+    Makes the app object with the configuration from
+    an environment variable and connects routes and mqtt
+    to the app
+    """
+    app = Flask(__name__)
+    app.config.from_envvar('MYAPP_SETTINGS')
 
-mqtt.init_app(app)
+    mqtt.init_app(app)
 
-routes.init_app(app)
-
+    routes.init_app(app)
+    return app
 
 
